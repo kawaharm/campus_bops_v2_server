120c29b1ec6aa16c25dccdc6b2991da1067b4b4b
kawaharm <mjbk295@gmail.com>
2022-03-20 15:54:36 -0700

    updating song search

--- controllers/songs.js
+++ controllers/songs.js
@@ -52,6 +52,69 @@ router.get("/:id", (req, res) => {
         })
 });
 
+// SEARCH BY SONG
+router.get('/search', (req, res) => {
+    // Make a AXIOS call (POST) to submit CLIENT_ID and CLIENT_SECRET
+    axios.post('https://accounts.spotify.com/api/token',
+        querystring.stringify({ grant_type: 'client_credentials' }),
+        {
+            headers: headers
+        })
+        .then(response => {
+            token = response.data.access_token
+            console.log('TOKEN', token);
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }
+
+            console.long('song title: ', req.query.title)
+            console.long('song artist: ', req.query.artist)
+            console.long('song album: ', req.query.album)
+
+
+            // // Define song variable using value from song search bar
+            // let track = req.query.song;
+
+            // // Make another axios (GET) to retrieve song data 
+            // axios.get(`https://api.spotify.com/v1/search?q=${track}&type=track&offset=0&limit=5`, config)
+            //     .then(response => {
+            //         if (response) {
+            //             let items = response.data.tracks.items; // Array of songs data
+            //             let songArray = []; // Array of obj containing songs data
+
+            //             // Encapsulate each song data into an object and push into songArray
+            //             for (const item of items) {
+            //                 let song = {};
+            //                 const songTitle = item.name;
+            //                 const artists = item.artists.map(artist => artist.name);    // Map artist array to obtain all artists in song 
+            //                 const albumName = item.album.name;
+            //                 const songPlayerId = item.id;   // For embedded player
+            //                 song.title = songTitle;
+            //                 song.artist = artists;
+            //                 song.album = albumName;
+            //                 song.songPlayerId = songPlayerId;
+            //                 songArray.push(song);
+            //             }
+
+            //             res.status(200).json
+            //         }
+            //         else {
+            //             console.log('This song does not exist');
+            //             // render a 404 page
+            //             res.render('404', { message: 'This song does not exist' });
+            //         }
+            //     })
+            //     .catch(err => {
+            //         console.log(err);
+            //     });
+
+        })
+        .catch(err => {
+            console.log(err)
+        })
+});
 
 /**
  * POST ROUTES
