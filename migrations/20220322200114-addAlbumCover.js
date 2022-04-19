41d1ef7220a41f51e272a1de78cc1e8a678bd254
kawaharm <mjbk295@gmail.com>
2022-04-19 15:08:54 -0700

    debugging heroku migration issues

--- migrations/20220322200114-addAlbumCover.js
+++ migrations/20220322200114-addAlbumCover.js
@@ -9,8 +9,8 @@ module.exports = {
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
     return queryInterface.addColumn(
-      'Songs', // name of Source table (not model)
-      'albumCover', // name of the key we're adding 
+      'songs', // name of Source table (not model)
+      'albumCover', // name of the key we're adding
       {
         type: Sequelize.STRING,
         onUpdate: 'CASCADE',
@@ -27,8 +27,8 @@ module.exports = {
      * await queryInterface.dropTable('users');
      */
     return queryInterface.removeColumn(
-      'Songs', // name of table 
+      'songs', // name of table
       'albumCover' // key we want to remove
     );
-  }
+  },
 };
