10aaa94203b8ca0e63d5e96e2f469bf4a46b94b5
kawaharm <mjbk295@gmail.com>
2022-04-19 18:43:00 -0700

    debugging

--- server.js
+++ server.js
@@ -1,40 +1,43 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
 const app = express();
-const db = require("./db");
-const cors = require("cors");
+const db = require('./db');
+const cors = require('cors');
 const querystring = require('querystring');
-const {
-    School,
-    Category,
-    User,
-    Song
-} = require('./models');
+const { School, Category, User, Song } = require('./models');
 
 // Code for Spotify API
-let buff = new Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`);
+let buff = new Buffer.from(
+  `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`
+);
 let authKey = buff.toString('base64');
 let headers = {
-    Authorization: `Basic ${authKey}`
-}
+  Authorization: `Basic ${authKey}`,
+};
 
 // Logs response status and time (ms)
 // app.use(morgan("dev"));
 
 // Allow CORS
-app.use(cors())
+app.use(cors());
 // Convert client's response from JSON to Javascript object
 app.use(express.json());
-
 
 // CONTROLLERS
 app.use('/api/v1/schools', require('./controllers/schools'));
 app.use('/api/v1/categories', require('./controllers/categories'));
 app.use('/api/v1/songs', require('./controllers/songs'));
 
+if (process.env.NODE_ENV) {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 3006;
 app.listen(port, () => {
-    console.log(`You are listening on PORT ${port}`)
+  console.log(`You are listening on PORT ${port}`);
 });
