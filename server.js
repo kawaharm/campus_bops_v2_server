8ebc789a406d1fbcf4143d7816e422cc540bb05e
kawaharm <mjbk295@gmail.com>
2022-04-19 20:45:20 -0700

    debug

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
-const db = require('./db');
-const cors = require('cors');
-const querystring = require('querystring');
-const { School, Category, User, Song } = require('./models');
+const db = require("./db");
+const cors = require("cors");
+const path = require("path");
+const querystring = require("querystring");
+const { School, Category, User, Song } = require("./models");
 
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 
 // Code for Spotify API
 let buff = new Buffer.from(
   `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`
 );
-let authKey = buff.toString('base64');
+let authKey = buff.toString("base64");
 let headers = {
   Authorization: `Basic ${authKey}`,
 };
@@ -29,10 +30,10 @@ let headers = {
 
 app.use(cors());
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET', 'POST');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET", "POST");
   next();
 });
 
@@ -40,25 +41,25 @@ app.use(function (req, res, next) {
 app.use(express.json());
 
 // CONTROLLERS
-app.use('/api/v1/schools', require('./controllers/schools'));
-app.use('/api/v1/categories', require('./controllers/categories'));
-app.use('/api/v1/songs', require('./controllers/songs'));
+app.use("/api/v1/schools", require("./controllers/schools"));
+app.use("/api/v1/categories", require("./controllers/categories"));
+app.use("/api/v1/songs", require("./controllers/songs"));
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   app.use(cors());
   app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET', 'POST');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET", "POST");
     next();
   });
 
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, "client/build")));
   // Handle React routing, return all requests to React app
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
