5a06c71a6ba1f0f3c1566249b503c47034e6df32
kawaharm <mjbk295@gmail.com>
2022-04-21 09:02:37 -0700

    changed express.json to express.static in server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(cors());
 // });
 
 // Serve static files from the React app
-app.use(express.json(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 // CONTROLLERS
 app.use("/api/v1/schools", require("./controllers/schools"));
