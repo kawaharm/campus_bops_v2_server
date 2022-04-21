dd359ebfebd35c51d53a9b03ed62e22bd4cde6de
kawaharm <mjbk295@gmail.com>
2022-04-21 10:58:43 -0700

    revised server.js

--- server.js
+++ server.js
@@ -31,12 +31,12 @@ app.use(express.static(path.join(__dirname, "client/build")));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "client/build/index.html"));
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
   });
 }
 //BUILD mode
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/public/index.html"));
+  res.sendFile(path.join(__dirname, "client/public/index.html"));
 });
 
 // CONTROLLERS
