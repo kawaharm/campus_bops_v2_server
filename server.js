809bbdc9f3075192d18ea060621b6997d43f0871
kawaharm <mjbk295@gmail.com>
2022-04-21 10:51:56 -0700

    debug server.js

--- server.js
+++ server.js
@@ -23,15 +23,6 @@ let headers = {
 // // Allow CORS for client-side requests
 app.use(cors());
 
-// app.use(cors());
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   res.header("Access-Control-Allow-Methods", "GET", "POST");
-//   next();
-// });
-
 // Serve static files from the React app
 // *** Using express.json will cause Manifest: Line: 1, column: 1, Syntax error. ***
 app.use(express.static(path.join(__dirname, "client/build")));
@@ -52,22 +43,6 @@ app.get("*", (req, res) => {
 app.use("/api/v1/schools", require("./controllers/schools"));
 app.use("/api/v1/categories", require("./controllers/categories"));
 app.use("/api/v1/songs", require("./controllers/songs"));
-
-// if (process.env.NODE_ENV === "production") {
-//   // Serve any static files
-//   app.use(express.static(path.join(__dirname, "build")));
-//   // Handle React routing, return all requests to React app
-//   app.get("*", function (req, res) {
-//     res.sendFile(path.join(__dirname, "build", "index.html"));
-//   });
-// }
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
-});
-// app.get("*", (req, res) => {
-//   res.sendFile(__dirname + "/client/public/index.html");
-// });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
