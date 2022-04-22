0e297f7eaeb1f6c0f188e9f7b8381e40fef857ff
kawaharm <mjbk295@gmail.com>
2022-04-22 16:56:20 -0700

    test

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use("/api/v1/songs", require("./controllers/songs"));
 //PRODUCTION mode
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("/", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "client/build/index.html"));
   });
 }
