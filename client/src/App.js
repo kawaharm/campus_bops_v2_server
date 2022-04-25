0f3892707dc91623ef29d2a11160f8fd28aa8ef9
kawaharm <mjbk295@gmail.com>
2022-04-24 21:37:44 -0700

    updated navbar

--- client/src/App.js
+++ client/src/App.js
@@ -12,19 +12,16 @@ import SongSearch from "./routes/SongSearch";
 function App() {
   return (
     <SchoolsContextProvider>
-      {/* container class will give page side margins */}
-      <div className="container">
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path="/schools/" element={<Schools />} />
-            <Route path="/schools/:id" element={<SchoolDetail />} />
-            <Route path="/categories/:id/" element={<CategoryBracket />} />
-            <Route path="/search" element={<SongSearch />} />
-          </Routes>
-        </Router>
-      </div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/schools/" element={<Schools />} />
+          <Route path="/schools/:id" element={<SchoolDetail />} />
+          <Route path="/categories/:id/" element={<CategoryBracket />} />
+          <Route path="/search" element={<SongSearch />} />
+        </Routes>
+      </Router>
     </SchoolsContextProvider>
   );
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -36,7 +36,8 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            CAMPUS BOPS <i class="fa-solid fa-radio navbar-logo-icon" />
+            CAMPUS BOPS
+            <i className="fa-solid fa-radio navbar-logo-icon" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
